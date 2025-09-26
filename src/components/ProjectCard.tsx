8d3418723543538a1f02a2c3ceabb33a2dace54d
Biswajit Sahoo <biswatian123@gmail.com>
2025-09-26 20:41:51 +0530

    Added Live website link for mobile devices

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,6 +2,7 @@
 import { motion } from "framer-motion";
 import Image, { StaticImageData } from "next/image";
 import { PinContainer } from "./ui/3d-pin";
+import { SquareArrowOutUpRight } from "lucide-react";
 
 type ProjectCardProps = {
   image: string | StaticImageData;
@@ -41,17 +42,30 @@ const ProjectCard = ({
           </h3>
           <p className="text-sm text-gray-400 font-montserrat">{description}</p>
 
-          {/* Tech Icons */}
-          <div className="flex gap-3 text-xl text-gray-300 mt-3">
-            <p className="text-sm text-gray-200 font-sans">Tech Stack :</p>
-            {techIcons.map((icon, idx) => (
-              <span
-                key={idx}
-                className="hover:text-[#00f6ff] transition-colors duration-300"
-              >
-                {icon}
-              </span>
-            ))}
+          {/* Tech Icons and link*/}
+          <div className="flex gap-2 items-baseline justify-between">
+            <div className="flex gap-3 md:text-xl text-gray-300 mt-3">
+              <p className="text-xs md:text-sm text-gray-200 font-sans">
+                Tech Stack :
+              </p>
+              {techIcons.map((icon, idx) => (
+                <span
+                  key={idx}
+                  className="hover:text-[#00f6ff] transition-colors duration-300"
+                >
+                  {icon}
+                </span>
+              ))}
+            </div>
+            <a
+              className="text-xs bg-gradient-to-b from-black via-gray-900 to-gray-800 rounded-full px-2 py-1 flex gap-2 md:hidden font-sans items-center"
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Website
+              <SquareArrowOutUpRight className="size-3" />
+            </a>
           </div>
         </div>
       </motion.div>
