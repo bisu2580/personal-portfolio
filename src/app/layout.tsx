cd91378da16a1978a521a42e0005158996b04195
Biswajit Sahoo <biswatian123@gmail.com>
2025-12-06 16:11:14 +0530

    Added SEO functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,48 @@ const montserrat = Montserrat({
 
 export const metadata: Metadata = {
   title: "Portfolio Website - Biswajit Sahoo",
-  description: "This is a portfolio website of Biswajit",
+  description:
+    "Full-stack and MERN developer skilled in React, Next.js, Node.js, MongoDB, Web Apps, and Cloud deployment. Explore projects, resume, skills & contact.",
+  keywords: [
+    "Biswajit Sahoo",
+    "Fullstack Developer",
+    "Web Developer",
+    "MERN Stack",
+    "React Developer",
+    "Node.js Developer",
+    "Web Developer Portfolio",
+    "JavaScript",
+    "MongoDB",
+    "Express",
+    "Firebase",
+  ],
+  authors: [{ name: "Biswajit Sahoo" }],
+  creator: "Biswajit Sahoo",
+  publisher: "Biswajit Sahoo",
+  openGraph: {
+    title: "Biswajit Sahoo | Full-Stack Developer Portfolio",
+    description:
+      "Full-stack MERN developer building scalable applications with modern UI and optimized backend.",
+    url: "https://biswajit-sahoo-portfolio.vercel.app",
+    siteName: "Biswajit Sahoo Portfolio",
+    locale: "en_IN",
+    type: "website",
+    images: [
+      {
+        url: "https://biswajit-sahoo-portfolio.vercel.app/banner-hd.png",
+        width: 1584,
+        height: 396,
+      },
+    ],
+  },
+  metadataBase: new URL("https://biswajit-sahoo-portfolio.vercel.app"),
+  alternates: {
+    canonical: "https://biswajit-sahoo-portfolio.vercel.app",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
